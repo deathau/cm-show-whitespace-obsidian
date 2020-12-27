ec8cd60af9c5d8e10cb6fdefcf4cfd520efd9b47
Josh <8007967+jjspace@users.noreply.github.com>
2020-12-27 14:43:37 -0500

    add settings, extra styles for single/trailing spaces

--- main.ts
+++ main.ts
@@ -1,16 +1,35 @@
 import './styles.scss'
 import './cm-show-invisibles'
-import { Plugin, MarkdownView } from 'obsidian';
+import { Plugin, MarkdownView, PluginSettingTab, App, Setting } from 'obsidian';
 
+interface CMShowWhitespacePluginSettings {
+  enabled: boolean;
+  showNewline: boolean;
+  showTab: boolean;
+  showSpace: boolean;
+  showSingleSpace: boolean;
+  showTrailingSpace: boolean;
+}
+
+const DEFAULT_SETTINGS: CMShowWhitespacePluginSettings = {
+  enabled: true,
+  showNewline: true,
+  showTab: true,
+  showSpace: true,
+  showSingleSpace: true,
+  showTrailingSpace: true,
+};
+    
 export default class CMShowWhitespacePlugin extends Plugin {
 
-  settings: any;
+  settings: CMShowWhitespacePluginSettings;
   async onInit() {
     
   }
 
   async onload() {
-    this.settings = await this.loadData() || { enabled: true } as any;
+    await this.loadSettings();
+
     if (this.settings.enabled) {
       (this.app.workspace as any).layoutReady ? this.enable() : this.app.workspace.on('layout-ready', this.enable);
     }
@@ -24,6 +43,8 @@ export default class CMShowWhitespacePlugin extends Plugin {
         this.settings.enabled ? this.disable() : this.enable();
       }
     });
+
+    this.addSettingTab(new CMShowWhitespacePluginSettingTab(this.app, this));
   }
 
   onunload() {
@@ -63,5 +84,127 @@ export default class CMShowWhitespacePlugin extends Plugin {
   showInvisibles = (cm: CodeMirror.Editor, enable: boolean = true) => {
     // @ts-ignore
     cm.setOption("showInvisibles", enable);
+  }
+
+  updateHiddenChars = () => {
+    const { showNewline, showSingleSpace, showSpace, showTab, showTrailingSpace } = this.settings;
+    const body = document.body;
+
+    if (showNewline) {
+      body.classList.remove('plugin-cm-show-whitespace-hide-newline');
+    } else {
+      body.classList.add('plugin-cm-show-whitespace-hide-newline');
+    }
+    if (showTab) {
+      body.classList.remove('plugin-cm-show-whitespace-hide-tab');
+    } else {
+      body.classList.add('plugin-cm-show-whitespace-hide-tab');
+    }
+    if (showSpace) {
+      body.classList.remove('plugin-cm-show-whitespace-hide-space');
+    } else {
+      body.classList.add('plugin-cm-show-whitespace-hide-space');
+    }
+    if (showSingleSpace) {
+      body.classList.remove('plugin-cm-show-whitespace-hide-single-space');
+    } else {
+      body.classList.add('plugin-cm-show-whitespace-hide-single-space');
+    }
+    if (showTrailingSpace) {
+      body.classList.remove('plugin-cm-show-whitespace-hide-trailing-space');
+    } else {
+      body.classList.add('plugin-cm-show-whitespace-hide-trailing-space');
+    }
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign(DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+    this.updateHiddenChars();
+  }
+}
+
+class CMShowWhitespacePluginSettingTab extends PluginSettingTab {
+  plugin: CMShowWhitespacePlugin;
+
+  constructor(app: App, plugin: CMShowWhitespacePlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+    let {
+      containerEl,
+      plugin: { settings },
+    } = this;
+
+    containerEl.empty();
+    containerEl.classList.add('plugin-cm-show-whitespace-settings');
+
+    new Setting(containerEl).setHeading().setName('Spaces');
+    new Setting(containerEl)
+      .setName('Show space characters')
+      .setDesc(
+        'Show or hide the space character. Note: This will also hide single space characters.'
+      )
+      .addToggle((toggle) =>
+        toggle.setValue(this.plugin.settings.showSpace).onChange(async (value) => {
+          console.log('show space has changed', value);
+          this.plugin.settings.showSpace = value;
+          this.plugin.settings.showSingleSpace = value;
+          await this.plugin.saveSettings();
+          this.display();
+        })
+      );
+    const singleSpaceSetting = new Setting(containerEl)
+      .setName('Show single space characters')
+      .setDesc('Show or hide single space characters')
+      .addToggle((toggle) => {
+        toggle.setValue(this.plugin.settings.showSingleSpace).onChange(async (value) => {
+          console.log('show single space has changed', value);
+          this.plugin.settings.showSingleSpace = value;
+          await this.plugin.saveSettings();
+        });
+      });
+    if (!settings.showSpace) {
+      // if general spaces are off it doesn't make sense to change the setting
+      // to show or hide single spaces between words
+      singleSpaceSetting.setClass('plugin-cm-show-whitespace-disabled');
+    }
+    new Setting(containerEl)
+      .setName('Show trailing space characters')
+      .setDesc('Show or hide trailing space characters')
+      .addToggle((toggle) =>
+        toggle.setValue(this.plugin.settings.showTrailingSpace).onChange(async (value) => {
+          console.log('show trailing space has changed', value);
+          this.plugin.settings.showTrailingSpace = value;
+          await this.plugin.saveSettings();
+        })
+      );
+
+    new Setting(containerEl).setHeading().setName('Other whitespace characters');
+    new Setting(containerEl)
+      .setName('Show newline characters')
+      .setDesc('Show or hide the newline character')
+      .addToggle((toggle) =>
+        toggle.setValue(this.plugin.settings.showNewline).onChange(async (value) => {
+          console.log('show newline has changed', value);
+          this.plugin.settings.showNewline = value;
+          await this.plugin.saveSettings();
+        })
+      );
+    new Setting(containerEl)
+      .setName('Show tab characters')
+      .setDesc('Show or hide the tab character')
+      .addToggle((toggle) =>
+        toggle.setValue(this.plugin.settings.showTab).onChange(async (value) => {
+          console.log('show tab has changed', value);
+          this.plugin.settings.showTab = value;
+          await this.plugin.saveSettings();
+        })
+      );
   }
 }
